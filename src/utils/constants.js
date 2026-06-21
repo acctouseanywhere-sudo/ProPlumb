760deca52039f6b48b581051f6d40512c0f09cc4
acctouseanywhere-sudo <noreply@github.com>
2026-06-21 17:01:23 +0530

    Update map embed to Rothesay, Isle of Bute

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,6 +7,6 @@ export const NAV_LINKS = [
 ]
 
 export const MAP_EMBED =
-  'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50000!2d-73.9857!3d40.7484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a165bedccab%3A0x2cb2ddf003b6ae0d!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1'
+  'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22434.585721774002!2d-5.08!3d55.835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x488fcb8ba3e7e801%3A0x4aca07b1f6dd2d2b!2sRothesay%2C%20Isle%20of%20Bute!5e0!3m2!1sen!2suk!4v1'
 
 
